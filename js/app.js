9257f720cf2b7fa9c0654d0fcc696930d6c30653
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-27 11:32:23 -0300

    agregamos ejemplo de import

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//import {Contacto, prueba} from "./classContacto.js";
+import Contacto from "./classContacto.js";
+
+//variables globales
+//const contactoNuevo = new Contacto(1,'algun nombre', 'apellido', 'dsfdkdfd@.com', 4454566767);
+const agenda = [];
+const formularioContacto = document.querySelector('form');
+console.log(formularioContacto);
+
+// funciones
+const crearContacto = ()=>{
+    
+}
+
+// logica
+
+
+
